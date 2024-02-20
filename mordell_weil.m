b2c448a58df796fad88afb80a8ca76e9e94c15a9
Angelos Koutsianas <koutsis.jr@gmail.com>
2024-02-20 05:15:22 -0800

    - Models for Xsp19 from LMFDB - A file with the Mordell-Weil groups of the curves - Code that we can use to prove that Pi - Pj are linear independent

--- mordell_weil.m
+++ mordell_weil.m
@@ -1,40 +1,227 @@
-// In this file we determine a finite index subgroup of JD10 orver the rationals.
-
-P<x,y,z,u,v,w> := ProjectiveSpace(Rationals(),5);
-
-ID10 := [u*w - 2*v*w + 2*u*x - 6*v*x + 2*u*y + 2*v*y + u*z,
-u*w + v*w + 2*u*x - 2*v*x + 2*u*y - 10*v*y - 5*u*z + 11*v*z,
-- 6*u^2 + 6*u*v - 3*v^2 + 11*w^2 - 66*w*x + 11*x^2 + 88*w*y  - 110*x*y + 99*y^2 + 44*w*z - 110*x*z,
-6*u^2 + 12*u*v + 12*v^2 + 187*w*x + 22*x^2 + 55*w*y - 44*x*y - 154*y^2 + 66*w*z + 77*x*z  + 121*y*z,
-- 9*v^2 + 88*w^2- 11*w*x -99*x^2 - 77*w*y + 110*x*y - 11*y^2 + 77*w*z - 297*x*z  + 121*y*z,
-- 6*u^2 - 12*u*v - 12*v^2 + 33*w^2 - 77*w*x + 66*x^2 - 121*w*y - 132*x*y - 110*y^2 - 44*w*z - 187*x*z + 121*y*z  + 121*z^2];
-XD10 := Curve(P, ID10);
-SD10 := Scheme(P, ID10);
-phi := iso<XD10 -> XD10 | [-P.1, -P.2, -P.3, P.4, P.5, -P.6],[-P.1, -P.2, -P.3, P.4, P.5, -P.6]>;
-G := AutomorphismGroup(XD10,[phi]);
-C, XD10ToC := CurveQuotient(G);
-CQQ := [C![1, -3, 1], C![1, -1, 1], C![0, -1, 1], C![0, 0, 1], C![1, -1, 0], C![1, 0 , 0]];
-JC := Jacobian(C);
-GJC, GJCToJC := MordellWeilGroup(JC);
-JCToGJC := Inverse(GJCToJC);
+// In this file we determine a finite index subgroup of the Mordell-Weil group of the Jacobian of the modular curves.
 
 
 
-// Compute points on JC over K = Q(sqrt(-11))
+// Useful functions
 
-K<r11> := QuadraticField(-11);
-CK := BaseChange(C, K);
-JK := Jacobian(CK);
-_<s> := PolynomialRing(K);
-Csim, CToCsim := SimplifiedModel(C);
-Jsim := Jacobian(Csim);
-G2 := CK![1,0,0] - CK![1,-1,0];
-G3 := JK![s^2 + s + 1, (-r11 - 1)/2];
 
-CsimK := BaseChange(Csim, K);
-JsimK := Jacobian(CsimK);
-G2sim := CsimK!(CToCsim(C![1,0,0])) - CsimK!(CToCsim(C![1,-1,0]));
-G3sim := JsimK![s^2 + s + 1, -r11];
+function ZetaFunctionNumerator(X, p, genus)
+// intrinsic ZetaFunctionNumerator(X::Crv, p::RngIntElt, genus::RngIntElt) -> RngUPolElt
+//
+//	X: A model of the curve over the Q
+//	p: a prime of good reduction
+//	genus: the genus of X
+
+
+	////////// We may not need it ///////////
+
+	PS<t> := PowerSeriesRing(Rationals(), genus + 1);
+	R<T> := PolynomialRing(Rationals());
+
+	f := PS!0;
+	for i in [1..genus] do
+		bi := #Points(ChangeRing(X, FiniteField(p^i)));
+		f +:= bi * t^i/i;
+		//printf "%o\n", f;
+	end for;
+	F := (1 - t) * (1 - p*t) * Exp(f);
+	printf "F: %o\n", F;	
+	
+	P1 := R!1 + p^genus * T^(2*genus);
+	for i in [1..genus] do
+		ai := Coefficient(F, i);
+		P1 +:= ai*T^i;
+		if i ne genus then
+			P1 +:= ai * p^(genus-i) * T^(2*genus-i);		
+		end if;
+	end for;
+
+	return P1;
+end function;
+//end intrinsic;
+
+
+function TorsionOrderBound(X, primes)
+
+	L := [];
+	for p in primes do
+		Xp := ChangeRing(X, GF(p));
+		if IsNonSingular(Xp) then
+			P1 := Numerator(ZetaFunction(ChangeRing(X, FiniteField(p))));
+			Append(~L, Evaluate(P1, 1));
+		else
+			printf "X is not singular at %o.\n", p;
+		end if;
+	end for;
+
+	return GCD(L);
+
+end function;
+
+
+
+function TorsionSubgroupOfJacobian(X, p)
+
+	Xp := ChangeRing(X, GF(p));
+	is_singular := false;
+	for i in [1..NumberOfAffinePatches(Xp)] do
+		if IsSingular(AffinePatch(Xp, i)) then
+			is_singular := true;
+			break;
+		end if;
+	end for;
+	if not is_singular then
+		H, phi, psi := ClassGroup(Xp);
+		C1 := FreeAbelianGroup(1);
+		deg := hom<H -> C1 | [Degree(phi(a)) * C1.1 : a in OrderedGenerators(H)]>;
+		Jp := Kernel(deg); 
+
+		return Jp;
+	else
+		printf "The curve is not singular at %o.\n", p;
+		return false;
+	end if;
+end function;
+
+
+
+intrinsic ReduceDivModp(Xp::Crv, D::DivSchElt) -> DivCrvElt
+{
+	
+	Xp: a curve over Fp
+	D: a 
+}
+	p := Characteristic(BaseRing(Xp));
+	Rp := CoordinateRing(AmbientSpace(Xp));
+	Dp := DivisorGroup(Xp) ! 0;
+	Fp := GF(p);
+	for Di in IdealFactorisation(D) do
+		I := Di[1];
+		ai := Integers()!Di[2];
+		Ip := [];
+		for f in Generators(I) do
+			val := AbsoluteValue(Min([Valuation(ci, p) : ci in Coefficients(f)]));
+			if val gt 0 then
+				Append(~Ip, Rp!(p^val * f));
+			else
+				Append(~Ip, Rp!f);
+			end if;
+		end for;
+
+		Dip := Divisor(Xp, Ideal(Ip));
+		Dp +:= ai * Dip;
+	end for;
+	
+	return Dp;
+end intrinsic;
+
+
+intrinsic LinearIndependent(X::Crv, divs::SeqEnum, primes::SeqEnum, torsion_order::RngIntElt) -> BoolElt
+{
+	X: a curve over Q
+	divs: a sequence of 0 rational divisors
+	primes: the primes of good reduction that we use
+	torsion_order: the order of the torsion divides this number
+
+	True in case we probe that divs a Z-linear idependent in J(Q)/Tor, otherwise false
+}
+	
+	G := FreeAbelianGroup(#divs);
+	T := G;
+	torsion_order_divs := PrimeFactors(torsion_order);
+
+	for p in primes do
+		Xp := ChangeRing(X, GF(p));
+		divsp := [ReduceDivModp(Xp, D) : D in divs];
+		Cp, psi, phi := ClassGroup(Xp);
+		GtoCp := hom<G -> Cp | [phi(Dip) : Dip in divsp]>;
+		T := T meet Kernel(GtoCp);
+		T := sub<G | [G!g : g in Generators(T)]>;
+		M := GCD([GCD(Eltseq(G!g)) : g in Generators(T)]);
+		printf "M:%o, T: %o\n", M, T;
+
+		if not(Set(PrimeDivisors(M)) subset torsion_order_divs) then
+			return true;
+		end if;
+	end for;
+
+	return false;
+
+end intrinsic;
+
+
+intrinsic ReducePtModp(Xp::Crv, P::Pt) -> Pt
+{
+	Xp: the reduction mod p of a projective curve X
+	P: a rational point of X(Q)
+
+	The reduction of P mod p
+}
+
+	Fp := BaseRing(Xp);
+	p := Characteristic(Fp);
+	val := AbsoluteValue(Min([Valuation(c, p) : c in Coordinates(P)]));
+	return Xp![Fp!(c*p^val) : c in Coordinates(P)];
+end intrinsic;
+
+
+intrinsic LinearIndependentFromPts(X::Crv, pts::SeqEnum, primes::SeqEnum, torsion_order::RngIntElt, r::RngIntElt) -> SeqEnum, BoolElt
+{
+	X: a curve over Q
+	pts: points on X(Q)
+	primes: a set of primes of good reduction that we use
+	torsion_order: the order of the torsion divides this number
+	r: The number of Pi - Pj that we want to prove that are Z-linear independent
+	
+}
+	subs := SetToSequence(Subsets(Set(pts), r + 1));
+	G := FreeAbelianGroup(r);
+	T := G;
+	torsion_order_divs := PrimeFactors(torsion_order);
+
+	SubsRec := recformat<points: SeqEnum, ker: GrpAb>;
+	CpRec := recformat<Xp: Crv, Cp: GrpAb, psi: Map, phi: Map>;
+
+	subs_recs := [];
+	for sub in subs do
+		rec := rec<SubsRec | points := SetToSequence(sub), ker := T>;
+		Append(~subs_recs, rec);
+	end for;
+
+	jac_recs := [];
+	for p in primes do
+		Xp := ChangeRing(X, GF(p));
+		Cp, psi, phi := ClassGroup(Xp);
+		rec := rec<CpRec | Xp := Xp, Cp := Cp, psi := psi, phi := phi>;
+		Append(~jac_recs, rec);
+	end for;
+	
+	for sub in subs_recs do
+		printf "sub: \n\n", sub;
+		points := sub`points;
+		T := sub`ker;
+		for jac in jac_recs do
+			Xp := jac`Xp;
+			printf "p: %o\n", Characteristic(BaseRing(Xp));
+			Cp := jac`Cp;
+			phi := jac`phi;
+			ptsmodp := [ReducePtModp(Xp, P) : P in points];
+			divsp := [Place(ptsmodp[i]) - Place(ptsmodp[r + 1]) : i in [1..r]];
+			GtoCp := hom<G -> Cp | [phi(Dip) : Dip in divsp]>;
+			T := T meet Kernel(GtoCp);
+			T := sub<G | [G!g : g in Generators(T)]>;
+			M := GCD([GCD(Eltseq(G!g)) : g in Generators(T)]);
+			printf "M:%o\n", M;
+			printf "T: %o\n\n", T;
+			
+			if not(Set(PrimeDivisors(M)) subset torsion_order_divs) then
+				return points, true;
+			end if;
+		end for;
+	end for;
+
+	return [], false;
+end intrinsic;
 
 
 
@@ -113,20 +300,4 @@ function LiftPointsJ(J, p : B := 2, d := 2)
 	end for;
 	return pts;
 end function;
-pts := LiftPointsJ(JK, 7);
 
-
-// Prove that the rank of JD10 over K is 2
-
-
-
-// Finite index subgroup of the JD10(Q)
-
-
-R := CoordinateRing(P);
-D1a := Divisor(SD10, Ideal([R.1*R.6 + R.6^2, R.2*R.6 - 7*R.6^2, R.3*R.6 - 5*R.6^2, R.4^2 - 473*R.6^2, R.5^2 - 473*R.6^2]));
-Dinf := Divisor(SD10, Ideal([R.1*R.6 + 1/3*R.6^2, R.2*R.6, R.3*R.6 + 1/3*R.6^2, R.4^2 - 22/9*R.6^2, R.5^2 - 22/9*R.6^2]));
-D1 := D1a - Dinf;
-
-D2a := Divisor(SD10, Ideal([R.1*R.6 - R.6^2, R.2*R.6 - R.6^2, R.3*R.6 - R.6^2, R.4^2 - 11*R.6^2, R.5^2 - 11*R.6^2]));
-D2 := D2a - Dinf;
